e9fafbb92941321c78927cbba0eafc41ba0d4760
Verhelst Damien <D.verhelst84@gmail.com>
2025-06-27 22:03:43 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+
+    // <editor-fold defaultstate="Collapsed" desc="Points d'entrée de l'applicaiton">
+    /**
+     * Points d'entrée de l'applicaiton
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // <editor-fold defaultstate="Collapsed" desc="Exo 1">
+
+        /**
+         * Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :
+                   <La valeur d'une variable de type int >
+                   Il y a quelques années cependant, elles étaient au nombre de :
+                  <La valeur de la MÊME VARIABLE>
+         */
+
+        int nbPlanetes = 8 ;
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : " +nbPlanetes +".");
+        nbPlanetes = 9;
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : " +nbPlanetes +".");
+        // </editor-fold>
+    }
+
+    // </editor-fold>
+
+}
